39d1611d1fec375af660b325464932b2630ac19f
sumit <sk7627577@gmail.com>
2024-07-12 20:50:56 +0530

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  // images: {
+  //   domains: [
+  //     "eazotel-client-images.s3.ap-south-1.amazonaws.com",
+  //     "cdn.builder.io",
+  //   ],
+  // },
   images: {
-    domains: [
-      "eazotel-client-images.s3.ap-south-1.amazonaws.com",
-      "cdn.builder.io",
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "eazotel-client-images.s3.ap-south-1.amazonaws.com",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.builder.io",
+      },
     ],
   },
 };
